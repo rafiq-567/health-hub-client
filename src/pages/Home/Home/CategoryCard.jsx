020373b1f4afb90fc8fa1ee684c05c135fb60c81
unknown <mdrafiqulakhter0@gmail.com>
2025-08-17 14:34:20 +0600

    added see more btn

--- src/pages/Home/Home/CategoryCard.jsx
+++ src/pages/Home/Home/CategoryCard.jsx
@@ -5,15 +5,17 @@ const CategoryCard = ({ category }) => {
     
 
   return (
-    <Link to={`/categories/${categoryName}`}>
+    
       <div className="rounded shadow p-4 hover:shadow-lg transition bg-white">
         <img src={imageURL} alt={name} className="w-full h-30 object-cover rounded" />
         <h3 className="text-lg font-semibold mt-2">{categoryName}</h3>
        
         <p className="text-md text-gray-600"><span className='font-bold'>Medicine Count:</span> {medicineCount} Medicines</p>
         <p className="text-md text-gray-600"><span className='font-bold'>Description:</span> {description} </p>
+        <Link to={`/categories/${categoryName}`}>
+        <button className='btn btn-primary mt-2'>See More</button></Link>
       </div>
-    </Link>
+    
   );
 };
 
